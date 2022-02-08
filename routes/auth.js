8e8a9d113cb7c239ee6d1e6e62c1814824e180f0
Dmitrij <Dmitrijkamynin2@gmail.com>
2022-02-08 16:15:08 +0300

    Status 401 if jwt old

--- routes/auth.js
+++ routes/auth.js
@@ -6,23 +6,13 @@ module.exports = async (req, res, next) => {
             console.log(req.headers.authorization);
             const decoded = jwt.verify(req.headers.authorization, 'chereshnia');
             console.log(decoded);
-            // const { name, password } = req.body;
-            // const thisUser = await db.User.findOne({
-            //     where: {
-            //         name: name,
-            //     }
-            // });
-            // if (!checkingUser) {
-            //     throw new Error('not such user');
-            // }
-            // const checkingPassword = (password === checkingUser.password);
-            // if (!checkingPassword) {
-            //     throw new Error('invalid password');
-            // }
-            // const token = jwt.sign(checkingUser.id, 'chereshnia');
-            // res.status(200).json(token);
             next();
         } catch(err) {
-            res.status(404).json(err.message);
+            console.log(err.message);
+            if (err.message === 'jwt expired') {
+                res.sendStatus(401);
+            } else {
+                res.status(404).json(err.message);
+            }
         }
     }
