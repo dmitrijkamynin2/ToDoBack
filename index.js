1fe523ff4e11abde9cbb76ebe839d9b3d34b2ce8
Dmitrij <Dmitrijkamynin2@gmail.com>
2022-02-09 13:01:28 +0300

    Add config into .env

--- index.js
+++ index.js
@@ -9,15 +9,6 @@ var fs = require('fs');
 require('dotenv').config()
 
 app.use(express.json());
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', '*');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     res.setHeader('Access-Control-Request-Headers', '*');
-//     next();
-// });
-
 app.use('*', cors());
 
 
